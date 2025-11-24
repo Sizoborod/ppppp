4750983c66c2b49f438bc30762c5085de4440140
Diivika <diivika2@yandex.ru>
2025-11-24 16:35:56 +0300

    update attack

--- units/items_.py
+++ units/items_.py
@@ -5,9 +5,9 @@ class Items:
     def __int__(self):
         pass
 
-    def attack(self):
-
-        return 0
+    def attack(self, attacker):
+        if attacker:
+            return True
 
     def defence(self):
         pass
